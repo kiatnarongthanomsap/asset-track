9d8cbcc8160a168daead86a39f713fa754087839
Kiatnarong T. <51500116+kiatnarongthanomsap@users.noreply.github.com>
2025-12-26 09:35:22 +0700

    ย้ายไป Supabase ITKUSCC

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vyeajefodhcwaanuexzg.supabase.co'
-const supabaseAnonKey = 'sb_publishable_mub-bnzYF8bBKnq2b2WXzw_PXKFmslc'
+const supabaseUrl = 'https://inwlvuavdfuabfayhwgo.supabase.co'
+const supabaseAnonKey = 'sb_publishable_sbOZnnDordhzUMqLVCmvWg_wlsLkvAB'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
